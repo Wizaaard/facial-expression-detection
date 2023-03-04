c94a86f36df005cbd6e2e436a8dd96edcd9fa75e
Ben Tamo <btamo@gatech.edu>
2023-03-04 14:30:56 -0500

    first comit

--- training.py
+++ training.py
@@ -4,4 +4,3 @@ from keras.layers import Conv2D, MaxPooling2D, Dense, Dropout, Flatten
 from keras.optimizers import Adam
 from keras.preprocessing.image import ImageDataGenerator
 
-print(keras.__version__)
